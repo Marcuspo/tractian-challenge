79ccc9ff6a8349ff1638c08cd6be1c01bdcfe2d1
Marcus Pontes <marcusporama@gmail.com>
2021-08-31 14:21:18 -0300

    create api.js, fix index.js in dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,22 +1,47 @@
-import { Card, Divider } from "antd"
+import { Card, Divider, Spin } from "antd"
 
 import { FullContainer } from "./styles"
 
+import api from "../api/api"
+import { useEffect, useState } from "react"
+
 function Index() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getData()
+  }, [data])
+
+  async function getData() {
+    const response = await api.get("/assets")
+    const docs = response.data
+
+    setData(docs)
+    setLoading(false)
+    console.log(data)
+  }
+
   return (
     <FullContainer>
-      <Card
-        style={{ width: 300 }}
-        cover={
-          <img
-            alt="card"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
-        }
-      >
-        Teste
-      </Card>
-      <Divider />
+      {loading ? (
+        <Spin />
+      ) : (
+        <>
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <img
+                alt="card"
+                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+              />
+            }
+          >
+            Teste
+          </Card>
+          <Divider />
+        </>
+      )}
     </FullContainer>
   )
 }
