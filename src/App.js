47c00de660537c69842e22edb7f03c0ff16ce4f9
Marcus Pontes <marcusporama@gmail.com>
2021-09-01 12:49:15 -0300

    add .env, update app.js for new styles, create styles.js for app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react"
 import Index from "./components/Dashboard/index"
 import NavBar from "./components/Navbar"
 
+import { GlobalStyle } from "./styles"
+
 const App = () => (
   <div className="App">
     <NavBar />
     <Index />
+    <GlobalStyle />
   </div>
 )
 
