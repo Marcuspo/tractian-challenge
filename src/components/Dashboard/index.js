8c0eac813e9f52ae127ada152758c6c39c79b501
Marcus Pontes <marcusporama@gmail.com>
2021-09-01 10:17:21 -0300

    create chart.js, fix in index.js, styles.js for adjust in chart div

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,17 +1,32 @@
-import { Card, Divider, Spin } from "antd"
+import { Card, Divider, Spin, Tag, Row, Col } from "antd"
 
-import { FullContainer } from "./styles"
+import {
+  FullContainer,
+  StyleCard,
+  DivChart,
+  InsideCard,
+  FirstContainer,
+} from "./styles"
+
+import {
+  ExclamationCircleOutlined,
+  SyncOutlined,
+  CloseCircleOutlined,
+} from "@ant-design/icons"
 
 import api from "../api/api"
 import { useEffect, useState } from "react"
 
+import Chart from "./Chart/Chart"
+
 function Index() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
+  const [units, setUnits] = useState([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getData()
-  }, [data])
+  }, [])
 
   async function getData() {
     const response = await api.get("/assets")
@@ -23,26 +38,73 @@ function Index() {
   }
 
   return (
-    <FullContainer>
-      {loading ? (
-        <Spin />
-      ) : (
-        <>
-          <Card
-            style={{ width: 300 }}
-            cover={
-              <img
-                alt="card"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-          >
-            Teste
-          </Card>
-          <Divider />
-        </>
-      )}
-    </FullContainer>
+    <>
+      <FirstContainer>Selecionar empresa:</FirstContainer>
+      <FullContainer>
+        {loading ? (
+          <Spin />
+        ) : (
+          <>
+            {data.map((data) => (
+              <div className="site-card-wrapper">
+                <Row gutter={4}>
+                  <Col span={10}>
+                    <StyleCard key={data.id}>
+                      <strong>{data.name}</strong>
+                      {data.status === "inAlert" ? (
+                        <Tag
+                          icon={<ExclamationCircleOutlined />}
+                          color="warning"
+                          className="cards"
+                        >
+                          In alert!!
+                        </Tag>
+                      ) : data.status === "inOperation" ? (
+                        <Tag
+                          icon={<SyncOutlined spin />}
+                          color="processing"
+                          className="cards"
+                        >
+                          In operation
+                        </Tag>
+                      ) : (
+                        <Tag
+                          icon={<CloseCircleOutlined />}
+                          color="error"
+                          className="cards"
+                        >
+                          In downtime
+                        </Tag>
+                      )}
+                      <InsideCard>
+                        <Card
+                          bordered={false}
+                          style={{ width: 300 }}
+                          cover={<img alt="card" src={data.image} />}
+                        >
+                          <DivChart>
+                            <Chart data={data.healthscore} className="chart" />
+                          </DivChart>
+                          Units:{" "}
+                          {data.unitId === 1 ? (
+                            <strong>"Unidade Jaguar"</strong>
+                          ) : (
+                            <strong>"Unidade Tobias"</strong>
+                          )}
+                          <br />
+                          Company: <strong>Empresa teste</strong>
+                        </Card>
+                      </InsideCard>
+                      <Divider />
+                    </StyleCard>
+                  </Col>
+                </Row>
+              </div>
+            ))}
+          </>
+        )}
+      </FullContainer>
+    </>
   )
 }
 
--- src/components/Dashboard/styles.js
+++ src/components/Dashboard/styles.js
@@ -1,8 +1,32 @@
 import styled from "styled-components"
 
+export const FirstContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+  font-size: 20px;
+`
+
 export const FullContainer = styled.div`
   margin: 10px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
 `
 export const StyleCard = styled.div`
+  .cards {
+    margin-bottom: 5px;
+  }
+`
+
+export const DivChart = styled.div`
+  display: flex;
   width: 100%;
+  flex-direction: column;
+`
+
+export const InsideCard = styled.div`
+  display: flex;
 `
